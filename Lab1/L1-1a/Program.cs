743e57aa9184b07890b09b7b51dea27365e74123
FurqanHun <125211961+FurqanHun@users.noreply.github.com>
2023-04-21 18:29:25 +0500

    L1-1a - used the nullable operator to provide a default value in null case

--- Lab1/L1-1a/Program.cs
+++ Lab1/L1-1a/Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-       string firstNumber, secondNumber; 
+       string? firstNumber, secondNumber; 
         int number1, number2, sum; 
         
         Console.Write("Please enter the first integer: ");
@@ -12,8 +12,8 @@ class Program
         Console.Write("\nPlease enter the second integer: ");
         secondNumber = Console.ReadLine();
         
-        number1 = Int32.Parse(firstNumber);
-        number2 = Int32.Parse(secondNumber);
+        number1 = Int32.Parse(firstNumber ?? "0");
+        number2 = Int32.Parse(secondNumber ?? "0");
         sum = number1 + number2;
 
         Console.WriteLine( "\nThe sum is {0}.", sum );
